d08621641be6c9a495b3d8f44dbd3db8593e6691
juancito.eth <juancitoeth@gmail.com>
2022-11-08 14:23:11 -0300

    improve styling

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 
+import Button from '@/components/buttons/Button';
 import UnstyledLink from '@/components/links/UnstyledLink';
 import NextImage from '@/components/NextImage';
 
 const links = [
-  { href: '/', label: 'Route 1' },
-  { href: '/', label: 'Route 2' },
+  { href: '/', label: 'Explore' },
+  { href: '/', label: 'Provide Insurance' },
+  { href: '/', label: 'Ask for Insurance' },
+  { href: '/', label: 'My Policies' },
 ];
 
 export default function Header() {
@@ -14,22 +17,23 @@ export default function Header() {
       <div className='layout flex h-14 max-w-screen-xl items-center justify-between'>
         <UnstyledLink href='/' className='font-bold hover:text-gray-600'>
           <NextImage
-            src='/logo.png'
+            src='/logo3.png'
             height='45'
-            width='305'
+            width='304'
             alt='Logo'
           ></NextImage>
         </UnstyledLink>
-        <nav>
-          <ul className='flex items-center justify-between space-x-4'>
+        <nav className='flex gap-6'>
+          <ul className='flex items-center justify-between space-x-6'>
             {links.map(({ href, label }) => (
-              <li key={`${href}${label}`}>
+              <li key={`${href}${label}`} className='font-bold'>
                 <UnstyledLink href={href} className='hover:text-gray-600'>
                   {label}
                 </UnstyledLink>
               </li>
             ))}
           </ul>
+          <Button variant='primary'>Connect Wallet</Button>
         </nav>
       </div>
     </header>
